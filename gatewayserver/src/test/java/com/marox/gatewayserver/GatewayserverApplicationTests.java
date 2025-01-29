1369c28d3c7c14778cf68efabd4aa94f9ed89f14
Marwan <maroelsays2013@gmail.com>
2025-01-29 21:01:05 +0200

    Initial commit - Added microservices

---
+++ gatewayserver/src/test/java/com/marox/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marox.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
